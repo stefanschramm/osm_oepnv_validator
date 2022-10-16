18ec0dbe990d4ddbcc56c75705172969feee4a62
stefanschramm <mail@stefanschramm.net>
2022-10-16 12:12:31 +0200

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 # get current data
-# wget -q http://download.geofabrik.de/openstreetmap/europe/germany/berlin.osm.pbf -O /var/www/user1/osm_oepnv_validator/data/berlin.osm.pbf
-/usr/bin/python /var/www/user1/osm_oepnv_validator/fetch_overpass_data.py /var/www/user1/osm_oepnv_validator/overpass_queries /var/www/user1/osm_oepnv_validator/data
+/usr/bin/python3 /var/www/user1/osm_oepnv_validator/fetch_overpass_data.py /var/www/user1/osm_oepnv_validator/overpass_queries /var/www/user1/osm_oepnv_validator/data
 
 # generate everything
-#/var/www/user1/osm_oepnv_validator/python_env/bin/python /var/www/user1/osm_oepnv_validator/generate.py
-/usr/bin/python /var/www/user1/osm_oepnv_validator/generate.py
+/usr/bin/python3 /var/www/user1/osm_oepnv_validator/generate.py
 
