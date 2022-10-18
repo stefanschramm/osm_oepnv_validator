0e91a82c58c9528cf1f863c0650281aa7289e5d8
stefanschramm <mail@stefanschramm.net>
2022-10-18 08:25:05 +0200

    Remove obsolete scripts

--- validate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# get current data
-/usr/bin/python3 /var/www/user1/osm_oepnv_validator/fetch_overpass_data.py /var/www/user1/osm_oepnv_validator/overpass_queries /var/www/user1/osm_oepnv_validator/data
-
-# generate everything
-/usr/bin/python3 /var/www/user1/osm_oepnv_validator/generate.py
-
